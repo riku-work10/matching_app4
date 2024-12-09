b4cbef8fbead34fd74d7c7e8e310bde6fb7c0836
riku-work10 <ukir19990530@gmail.com>
2024-12-09 20:54:46 +0900

    スワイプページ見た目整えまで

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root 'top#index'
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
 end
