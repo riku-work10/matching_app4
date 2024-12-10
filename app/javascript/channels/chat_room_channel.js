3181449f5e65e9ca286b3d4334f66c27f2e5a0e0
riku-work10 <ukir19990530@gmail.com>
2024-12-11 02:13:56 +0900

    Action Cable を使ったチャット機能の実装 2、見た目の整え

--- app/javascript/channels/chat_room_channel.js
+++ app/javascript/channels/chat_room_channel.js
@@ -11,19 +11,21 @@ const appRoom = consumer.subscriptions.create("ChatRoomChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-    return alert(data["chat_message"]);
+    const chatMessages = document.getElementById('chat-messages');
+    chatMessages.insertAdjacentHTML('beforeend', data['chat_message']);
   },
 
-  speak: function (chat_message) {
-    return this.perform("speak", { chat_message: chat_message });
-  },
+  speak: function(chat_message, chat_room_id) {
+    return this.perform('speak', { chat_message: chat_message, chat_room_id: chat_room_id });
+  }
 });
 
 if (/chat_rooms/.test(location.pathname)) {
   window.document.addEventListener("keydown", (event) => {
     if (event.target.className === "chat-room__message-form_textarea") {
       if (event.key == "Enter") {
-        appRoom.speak(event.target.value);
+        const chat_room_id = document.querySelector('textarea').dataset.chat_room_id;
+        appRoom.speak(event.target.value, chat_room_id);
         event.target.value = "";
         event.preventDefault();
       }
